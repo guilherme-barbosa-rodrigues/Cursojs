0de0bf15f6cc96a05a73d57d3017cce08d2dacd5
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2022-11-09 23:05:04 -0300

    Update

---
+++ Aula01/ex003.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log("Voce ultrapassou a velocidade")
+}
+else
+    console.log("Dirija sempre com o cinto de segurança!")
