108ef973414b966afe3dbc7b42c6404c57dedc57
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2022-11-11 22:59:10 -0300

    update

---
+++ Aula01/ex004.js
@@ -0,0 +1,11 @@
+var idade = 12
+console.log(`Voçe tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade <18 || idade > 65 ) {
+        console.log('Voto Opicional') 
+    } else {
+        console.log('Voto Obrigatorio')
+    }
+
+    
